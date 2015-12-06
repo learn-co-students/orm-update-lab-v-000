fe9a0a0ec54107f43078686169556a07f3d7b9df
frankNowinski <nowinski.frank@gmail.com>
2015-12-06 00:35:39 -0500

    Done.

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -29,7 +29,7 @@ describe "Student" do
     end
   end
 
-  describe "#create_table" do 
+  describe ".create_table" do 
     it 'creates the students table in the database' do
       Student.create_table 
       table_check_sql = "SELECT tbl_name FROM sqlite_master WHERE type='table' AND tbl_name='students';"
