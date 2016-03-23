2bfeed2af8793bddc6ea6c6a0cc58b3303c03a48
ShiLi007 <lishidaniel@gmail.com>
2016-03-23 17:26:21 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,28 +1,22 @@
 require_relative "../config/environment.rb"
 
 class Student
-  attr_accessor :id, :name, :grade
+    attr_accessor :name, :grade
+  attr_reader :id
+  
+  def initialize(id=nil, name, grade)
+    @id = id
+    @name = name
+    @grade = grade
+  end
 
   def self.new_from_db(row)
-  new_student = self.new  
-  new_student.id = row[0]
-  new_student.name =  row[1]
-  new_student.grade = row[2]
-  new_student 
+    id = row[0]
+    name = row[1]
+    grade = row[2]
+    self.new(id, name, grade)
   end
 
-  def self.all
-    sql = <<-SQL
-      SELECT *
-      FROM Students
-    SQL
- 
-    DB[:conn].execute(sql).map do |row|
-      self.new_from_db(row)
-    end
-    # retrieve all the rows from the "Students" database
-    # remember each row should be a new instance of the Student class
-  end
 
   def self.find_by_name(name)
     sql = <<-SQL
@@ -40,14 +34,14 @@ class Student
     # return a new instance of the Student class
   end
   
-  def save
+def save
     sql = <<-SQL
       INSERT INTO students (name, grade) 
       VALUES (?, ?)
     SQL
-
     DB[:conn].execute(sql, self.name, self.grade)
-  end
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+end
   
   def self.create_table
     sql = <<-SQL
@@ -66,45 +60,14 @@ class Student
     DB[:conn].execute(sql)
   end
 
+ def self.create(name,grade)
+  student = Student.new(name,grade)
+  student.save
+ end
 
- def self.count_all_students_in_grade_9
-    sql = <<-SQL
-    SELECT * FROM students
-      WHERE students.grade = 9
-    SQL
-    DB[:conn].execute(sql)
+def update
+    sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
   end
-
-
-def self.students_below_12th_grade
-  sql = <<-SQL
-    SELECT * FROM students
-      WHERE students.grade < 12
-    SQL
-    DB[:conn].execute(sql)
-end
-
-def self.first_student_in_grade_10
-  sql = <<-SQL
-    SELECT * FROM students
-      WHERE students.grade = 10
-      LIMIT 1
-    SQL
-     self.new_from_db(DB[:conn].execute(sql).first)
-end
-
-
-def self.first_x_students_in_grade_10(num)
-self.new_from_db(DB[:conn].execute("SELECT * FROM students WHERE students.grade = 10 ORDER BY students.id ASC LIMIT ?", num).first)
-end
-
-def self.all_students_in_grade_X(num)
-      sql = <<-SQL
-    SELECT * FROM students
-      WHERE students.grade = num
-    SQL
-    DB[:conn].execute(sql)
-end
-
 
 end
