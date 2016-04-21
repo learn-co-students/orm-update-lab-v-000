a9a5577ec3f1777472292b712a76fab904292603
Rami97 <rami-sebastian@web.de>
2016-04-21 05:01:10 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -2,7 +2,7 @@ require_relative "../config/environment.rb"
 
 class Student
   attr_accessor :name, :grade, :id
-  def initialize(name, grade, id=nil)
+  def initialize(id=nil, name, grade)
     @name = name
     @grade = grade
     @id = id
@@ -44,14 +44,13 @@ class Student
       new_song.id = row[0]
       new_song.name =  row[1]
       new_song.grade = row[2]
-    binding.pry
       new_song  # return the newly created instance
     end
 
   def self.find_by_name(name)
     sql = "SELECT * FROM students WHERE name = ?"
     result = DB[:conn].execute(sql, name)[0]
-    Student.new(result[1], result[0], result[2])
+    Student.new(result[0], result[1], result[2])
   end
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
