fdf9e885048a825b5335e33f8c91762e8e4ef622
jd2rogers2 <jd2rogers2@gmail.com>
2016-06-20 14:49:17 -0400

    donzo

--- lib/student.rb
+++ lib/student.rb
@@ -4,6 +4,82 @@ class Student
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
-  
+  attr_accessor :name, :grade
+  attr_reader :id
+
+  def initialize(name, grade, id=nil)
+    @name = name
+    @grade = grade
+    @id = id
+  end
+
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade,TEXT);
+    SQL
+
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = <<-SQL
+      DROP TABLE students;
+    SQL
+
+    DB[:conn].execute(sql)
+  end
+
+  def save
+    if self.id
+      update
+    else
+      sql = <<-SQL
+        INSERT INTO students (name, grade)
+        VALUES (?, ?)
+      SQL
+
+      DB[:conn].execute(sql, self.name, self.grade)
+
+      sql_2 = <<-SQL
+        SELECT id FROM students
+        WHERE name = ?;
+      SQL
+
+      @id = DB[:conn].execute(sql_2, self.name)[0][0]
+    end
+  end
+
+  def self.create(name, grade)
+    new_student = Student.new(name, grade)
+    new_student.save
+  end
+
+  def self.new_from_db(array_row)
+    new_student = self.new(array_row[1], array_row[2], array_row[0])
+  end
+
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT * FROM students
+      WHERE name = ?;
+    SQL
+
+    row_array = DB[:conn].execute(sql, name)[0]
+    new_student = Student.new(row_array[1], row_array[2], row_array[0])
+  end
+
+  def update
+    sql = <<-SQL
+    UPDATE students
+    SET name = ?, grade = ?
+    WHERE id = ?;
+    SQL
+
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
+
+  end
  
 end
