4e38401cd91f28593aab8fa480f650f70cece4a8
Elaine Miller <emiller7149@gmail.com>
2016-08-09 00:11:58 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -4,6 +4,9 @@ class Student
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
+attr_accessor :id, :name, :grade
+
+
 def initialize(id=nil, name, grade)
   @id = id
   @name = name
@@ -13,7 +16,7 @@ end
 def self.create_table
   sql = <<-SQL
   CREATE TABLE IF NOT EXISTS students (
-  id INTEGER PRIMARY KEY,
+    id INTEGER PRIMARY KEY,
     name TEXT,
     grade INTEGER
   )
@@ -33,6 +36,9 @@ def self.create_table
   end
 
   def save
+    if self.id
+      self.update
+    else
     sql = <<-SQL
     INSERT INTO students (name, grade) VALUES (?, ?)
     SQL
@@ -40,13 +46,13 @@ def self.create_table
     DB[:conn].execute(sql, self.name, self.grade)
 
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
-
   end
+end
 
 
 
-  def self.create(name:, grade:)
-    student = Student.new(name, grade)
+  def self.create(name, grade)
+    student = self.new(name, grade)
     student.save
     student
   end
@@ -54,20 +60,22 @@ def self.create_table
 
 
   def self.new_from_db(row)
-    new_student = self.new
-    new_student.id = row[0]
-    new_student.name = row[1]
-    new_student.grade = row[2]
-    new_student
+    student = self.new(row[0], row[1], row[2])
+    student
   end
 
   def self.find_by_name(name)
   sql = <<-SQL
   SELECT * FROM students WHERE name = ? LIMIT 1
   SQL
-  DB[:conn].execute(sql,name).map do |row|
+  DB[:conn].execute(sql, name).map do |row|
     self.new_from_db(row)
   end.first
+end
+
+def update
+  sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
+  DB[:conn].execute(sql, self.name, self.grade, self.id)
 end
 
 
