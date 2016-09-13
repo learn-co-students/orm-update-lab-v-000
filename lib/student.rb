03db149085a3d5ce25652480048f174a82d261ed
Lani1321 <lani.berry13@gmail.com>
2016-09-13 12:15:43 -0400

    add #save to insert a new row into db using attributes of given object, add .create to create a student with two attributes, name and grade, add .new_from_db to create a new student object

--- lib/student.rb
+++ lib/student.rb
@@ -21,5 +21,34 @@ attr_accessor :id, :name, :grade
     DB[:conn].execute(sql)
   end
 
+  def self.drop_table
+    sql = "DROP TABLE IF EXISTS students"
+    DB[:conn].execute(sql)
+  end
+
+  def save
+    if self.id
+      self.update
+    else
+      sql = <<-SQL
+        INSERT INTO students (name, grade) 
+        VALUES (?, ?)
+      SQL
+      DB[:conn].execute(sql, self.name, self.grade)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+    end
+  end
+
+  def self.create(name, grade)
+    student = Student.new(name, grade)
+    student.save
+  end
+
+  def self.new_from_db(row)
+    id = row[0]
+    name = row[1]
+    grade = row[2]
+    self.new(id, name, grade)
+  end
 
 end
