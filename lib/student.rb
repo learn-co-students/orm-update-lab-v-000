00adee1aa5093c1c2eb3b5841669dc8ac592f87b
Steven De Jesus <stevendejesus90@gmail.com>
2016-12-06 21:59:38 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,10 +1,11 @@
 require_relative "../config/environment.rb"
-
+require 'pry'
 class Student
 attr_accessor :id, :name, :grade
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
-  def initialize(id=nil,name, grade)
+  def initialize(id = nil, name, grade)
+    @id = id
     @name = name
     @grade = grade
   end
@@ -27,16 +28,10 @@ attr_accessor :id, :name, :grade
    DB[:conn].execute(sql)
  end
 
- def save
-   if self.id
-     self.update
-  else
-   sql = <<-SQL
-    INSERT INTO students (name, grade)
-    VALUES (?, ?)
-   SQL
-   DB[:conn].execute(sql, self.name, self.grade)
-   @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+
+ def self.create(name, grade)
+   student = Student.new(name, grade)
+   student.save
  end
 
  def update
@@ -44,6 +39,35 @@ attr_accessor :id, :name, :grade
    DB[:conn].execute(sql, self.name, self.grade, self.id)
  end
 
+ def save
+     if self.id
+       self.update
+    else
+       sql = <<-SQL
+        INSERT INTO students (name, grade)
+        VALUES (?, ?)
+       SQL
+       DB[:conn].execute(sql, self.name, self.grade)
+       @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+    end
  end
+
+def self.new_from_db(row)
+  id = row[0]
+  name = row[1]
+  grade = row[2]
+  student = self.new(id, name, grade)
+end
+
+def self.find_by_name(name)
+  sql = <<-SQL
+     SELECT *
+     FROM students
+     WHERE name = ?
+     LIMIT 1
+   SQL
+   row = DB[:conn].execute(sql,name)[0]
+   self.new_from_db(row)
+end
 
 end
