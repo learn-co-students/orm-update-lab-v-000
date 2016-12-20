953856aef4993c82c4dddd6b1c09a1931deea208
Senad Hadrovic <senadhadrovic@gmail.com>
2016-12-20 19:20:15 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -47,18 +47,27 @@ class Student
  end
 
  def self.new_from_db(row)
-  new_song = self.new()  # self.new is the same as running Song.new
-  new_song.id = row[0]
-  new_song.name =  row[1]
-  new_song.length = row[2]
-  new_song
+  new_student = self.new(row[0], row[1], row[2]) # self.new is the same as running Song.new
+  # new_student.id = row[0]
+  # new_student.name =  row[1]
+  # new_student.grade = row[2]
+
+  new_student
+
+
  end
 
  def self.find_by_name(name)
     sql = "SELECT * FROM students WHERE name = ?"
     result = DB[:conn].execute(sql, name)[0]
     s = Student.new(result[0], result[1], result[2])
-    binding.pry
+    s
+  end
+
+
+  def update
+    sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
   end
 
 
