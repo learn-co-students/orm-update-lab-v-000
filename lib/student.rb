4714c7654c87a2a6ad07beb24ca162eae3d0878e
Senad Hadrovic <senadhadrovic@gmail.com>
2016-12-20 18:34:26 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,9 +1,66 @@
 require_relative "../config/environment.rb"
+require 'pry'
 
 class Student
 
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]
+  attr_accessor :name, :grade, :id
+
+  def initialize(id=nil, name, grade)
+    @name = name
+    @grade = grade
+    @id = id
+  end
+
+  def self.create_table
+   sql =  <<-SQL
+     CREATE TABLE IF NOT EXISTS students (
+       id INTEGER PRIMARY KEY,
+       name TEXT,
+       grade TEXT
+       )
+       SQL
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = "DROP TABLE IF EXISTS students;"
+    DB[:conn].execute(sql)
+  end
+
+  def save
+    if self.id
+      self.update
+    else
+      sql = <<-SQL
+      INSERT INTO students (name, grade)
+      VALUES (?, ?)
+      SQL
+    DB[:conn].execute(sql, self.name, self.grade)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+    end
+  end
+
+  def self.create(name, grade)
+   song = Student.new(name, grade)
+   song.save
+   song
+ end
+
+ def self.new_from_db(row)
+  new_song = self.new()  # self.new is the same as running Song.new
+  new_song.id = row[0]
+  new_song.name =  row[1]
+  new_song.length = row[2]
+  new_song
+ end
+
+ def self.find_by_name(name)
+    sql = "SELECT * FROM students WHERE name = ?"
+    result = DB[:conn].execute(sql, name)[0]
+    s = Student.new(result[0], result[1], result[2])
+    binding.pry
+  end
+
 
 
 end
