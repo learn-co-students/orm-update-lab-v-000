59bb36bbcecee31b4f39368d0da9531dc74999ec
bentonwong <bentonwong@gmail.com>
2017-03-08 10:59:41 -0800

    added #update and fixed persistance issue with save by adding if/then statements

--- lib/student.rb
+++ lib/student.rb
@@ -33,14 +33,18 @@ class Student
   end
 
   def save
-    sql = <<-SQL
-      INSERT INTO students (name, grade)
-      VALUES (?, ?)
-    SQL
+    if self.id
+      self.update
+    else
+      sql = <<-SQL
+        INSERT INTO students (name, grade)
+        VALUES (?, ?)
+      SQL
 
-    DB[:conn].execute(sql, self.name, self.grade)
+      DB[:conn].execute(sql, self.name, self.grade)
 
-    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+    end
   end
 
   def self.create(name, grade)
@@ -66,8 +70,8 @@ class Student
   end
 
   def update
-    sql = "UPDATE students SET name = ?, grade = ?"
-    DB[:conn].execute(sql,self.name,self.grade)
+    sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
+    DB[:conn].execute(sql,self.name,self.grade,self.id)
   end
 
 end
