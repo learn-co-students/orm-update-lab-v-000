2be70e042334fe137c371f5c76ed8c745f5c761f
WilliamBarela <william.barela.dev@gmail.com>
2017-05-08 18:20:47 +0900

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -43,7 +43,8 @@ class Student
   end
 
   def self.new_from_db(row)
-    self.new(row[0], row[1], row[2])
+    id, name, grade = row
+    self.new(id, name, grade)
   end
 
   def self.find_by_name(name)
