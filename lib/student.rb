6a2d8bc17b0720c37b28f680483e24f7a159ad3d
Andrew Bunys <awbunys@gmail.com>
2017-05-24 13:42:08 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -2,8 +2,8 @@ require_relative "../config/environment.rb"
 
 class Student
 
-  attr_accessor :name, :grade
-  attr_reader :id
+  attr_accessor :name, :grade, :id
+  
 
   def initialize(id=nil, name, grade)
     @id = nil
@@ -50,7 +50,7 @@ end
   end
 
   def self.new_from_db(row)
-    new_student = Student.new(name, grade)
+    new_student = Student.new(row[0], row[1], row[2])
     new_student.id = row[0]
     new_student.name = row[1]
     new_student.grade = row[2]
