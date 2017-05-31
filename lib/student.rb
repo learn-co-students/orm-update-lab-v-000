d484b35493f9f01e9a82b35f562b713ac6ce3c87
curlywallst <nnoyes@comcast.net>
2017-05-31 13:54:18 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -11,11 +11,10 @@ class Student
   end
 
   def self.new_from_db(row)
-    new_student = Student.new(row)
-    new_student.id = row[0]
-    new_student.name = row[1]
-    new_student.grade = row[2]
-    new_student
+    id = row[0]
+    name = row[1]
+    grade = row[2]
+    new_student = Student.new(name, grade, id)
   end
 
 
