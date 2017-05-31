2d48beab067ef6d7640ffe68dc92461bab4fdf23
curlywallst <nnoyes@comcast.net>
2017-05-31 13:47:50 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -42,7 +42,7 @@ class Student
   end
 
   def self.drop_table
-    DB[:conn].execute("DROP TABLE students")
+    DB[:conn].execute("DROP TABLE IF EXISTS students")
   end
 
   def self.create(name, grade)
@@ -60,11 +60,11 @@ class Student
   end
 
   def self.new_from_db(row)
-    new_student = Student.new(row[1],row[2],row[0])
-    new_student.id = row[0]
-    new_student.name = row[1]
-    new_student.grade = row[2]
-    new_student
+    id = row[0]
+    name = row[1]
+    grade = row[2]
+
+    new_student = Student.new(name,grade,id)
   end
 
   def update
