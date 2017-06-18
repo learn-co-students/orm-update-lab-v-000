cfe35903f1fe1bda2745ffb21781599cc00f7a97
charlie lee <ctlee9096@gmail.com>
2017-06-18 02:53:05 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -29,49 +29,40 @@ class Student
       DB[:conn].execute(sql)
     end
 
+    def update
+         sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
+         DB[:conn].execute(sql, self.name, self.grade, self.id)
+    end
+
     def save
       if self.id
         self.update
       else
-      sql = <<-SQL
+        sql = <<-SQL
         INSERT INTO students (name, grade)
-        VALUES (?,?)
-      SQL
-
-      DB[:conn].execute(sql, self.name, self.grade)
-      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+        VALUES (?, ?)
+        SQL
+        DB[:conn].execute(sql, self.name, self.grade)
+        @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
       end
    end
 
-   def self.create(name:, grade:)
+   def self.create(name, grade)
      student = Student.new(name, grade)
      student.save
      student
    end
 
-  def update
-       sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
-       DB[:conn].execute(sql, self.name, self,grade, self.id)
-  end
-
   def self.new_from_db(row)
-    new_student = self.new#self.new is same as Student.new
-    new_student.id = row[0]
-    new_student.name = row[1]
-    new_student.length = row[2]
+    new_student = self.new(row[0], row[1], row[2])#self.new is same as Student.new
     new_student
   end
 
   def self.find_by_name(name)
-    sql = <<-SQL
-    "SELECT *
-    FROM students
-    WHERE name = ?
-    LIMIT = 1
-    "
-    SQL
+    sql = "SELECT * FROM students WHERE name = ? LIMIT 1"
+
     DB[:conn].execute(sql,name).map do |row|
-    self.new_from_db(row)
+      self.new_from_db(row)
     end.first
   end
 end
