6a94c340a922e2ffd95894075596490cf2b510d1
Chase Paules <cpaules@gmail.com>
2017-08-10 20:20:47 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,9 +1,114 @@
-require_relative "../config/environment.rb"
-
 class Student
+  attr_accessor :id, :name, :grade
 
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]
+  def initialize(name = nil, grade = nil, id = nil)
+    @name = name
+    @grade = grade
+    @id = id
+  end
 
+  def self.new_from_db(row)
+    # create a new Student object given a row from the database
+    student = Student.new
+    student.id = row.flatten[0]
+    student.name = row.flatten[1]
+    student.grade = row.flatten[2]
+    student
+  end
 
+  def self.all
+    # retrieve all the rows from the "Students" database
+    # remember each row should be a new instance of the Student class
+    sql = <<-SQL
+      SELECT *
+      FROM students
+    SQL
+    students = DB[:conn].execute(sql)
+    students.map do |student|
+      self.new_from_db(student)
+    end
+  end
+
+  def self.find_by_name(name)
+    # find the student in the database given a name
+    # return a new instance of the Student class
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE name = ?
+    SQL
+    student = DB[:conn].execute(sql, name)
+    self.new_from_db(student)
+  end
+
+  def self.count_all_students_in_grade_9
+    self.all.select { |student| student.grade.to_i == 9 }
+  end
+
+  def self.students_below_12th_grade
+    self.all.select { |student| student.grade.to_i < 12 }
+  end
+
+  def self.first_student_in_grade_10
+    self.all.detect { |student| student.grade.to_i == 10 }
+  end
+
+  def self.first_x_students_in_grade_10(x)
+    arr = self.all.select { |student| student.grade.to_i == 10 }
+    arr[0..x-1]
+  end
+
+  def self.all_students_in_grade_x(x)
+    self.all.select { |student| student.grade.to_i == x }
+  end
+
+  def update
+    sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
+  end
+
+  def self.create(name, grade)
+    unless DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0]
+      id = 1
+    else
+      id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+    end
+    sql = <<-SQL
+      INSERT INTO students (name, grade, id)
+      VALUES (?, ?, ?)
+    SQL
+
+    DB[:conn].execute(sql, name, grade, id)
+  end
+
+  def save
+    if self.id
+      self.update
+    else
+      sql = <<-SQL
+        INSERT INTO students (name, grade)
+        VALUES (?, ?)
+      SQL
+
+      DB[:conn].execute(sql, self.name, self.grade)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+    end
+  end
+
+  def self.create_table
+    sql = <<-SQL
+    CREATE TABLE IF NOT EXISTS students (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      grade TEXT
+    )
+    SQL
+
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = "DROP TABLE IF EXISTS students"
+    DB[:conn].execute(sql)
+  end
 end
