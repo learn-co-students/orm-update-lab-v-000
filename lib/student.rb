cc904befa88909333afa6971c7ac5adbd89b757e
jonathan <jonathanyi81@gmail.com>
2017-08-12 23:25:25 -0400

    initialize, create_table and drop_table are defined

--- lib/student.rb
+++ lib/student.rb
@@ -4,6 +4,35 @@ class Student
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
+  attr_accessor :name, :grade
+  attr_reader :id
 
+  def initialize(id = nil, name, grade)
+    @id = id
+    @name = name
+    @grade = grade
+  end # initalized
+
+  def self.create_table
+    sql = <<-SQL
+    CREATE TABLE students (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      grade INTEGER
+    )
+    SQL
+
+    DB[:conn].execute(sql)
+  end # create_table
+
+  def self.drop_table
+    sql = <<-SQL
+    DROP TABLE students
+    SQL
+
+    DB[:conn].execute(sql)
+  end # drop_table
+
+  #def save
 
 end
