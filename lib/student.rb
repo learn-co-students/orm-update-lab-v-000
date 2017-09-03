14cc09317b115cdc815fcac5b87dfc791909b723
github username <github email address>
2017-09-03 11:07:48 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -2,6 +2,60 @@ require_relative "../config/environment.rb"
 
 class Student
 
+  attr_accessor :id, :name, :grade
+
+  def initialize(id = nil, name, grade)
+    @id = id
+    @name = name
+    @grade = grade
+
+  end
+
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE students (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade TEXT
+      )
+    SQL
+
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    DB[:conn].execute("DROP TABLE students")
+  end
+
+  def save
+    if self.id
+      self.update
+    else
+      DB[:conn].execute("INSERT INTO students (name, grade) VALUES (?, ?)", self.name, self.grade)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+    end
+  end
+
+  def self.create(name, grade)
+    student = Student.new(name, grade)
+    student.save
+    student
+  end
+
+  def self.new_from_db(row)
+    student = Student.new(row[0], row[1], row[2])
+    student
+  end
+
+  def self.find_by_name(name)
+    row = DB[:conn].execute("SELECT * FROM students WHERE name = ?", name)[0]
+    self.new_from_db(row)
+  end
+
+  def update
+    DB[:conn].execute("UPDATE students SET name = ?, grade = ? WHERE id = ?", self.name, self.grade, self.id)
+  end
+
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 
