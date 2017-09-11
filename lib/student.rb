851f018c7d844913d6e1e291f289e2fe45c08566
Alexander Friedland <friedlandaj@gmail.com>
2017-09-11 01:36:43 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,4 +1,5 @@
 require_relative "../config/environment.rb"
+require 'pry'
 
 class Student
 
@@ -31,6 +32,36 @@ class Student
     SQL
     DB[:conn].execute(sql)
   end
+
+  def save
+    if self.id
+      self.update
+    else
+      sql = <<-SQL
+        INSERT INTO students (name, grade) VALUES (?, ?)
+      SQL
+      DB[:conn].execute(sql, self.name, self.grade)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+    end
+  end
+
+  def update
+    sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
+  end
+
+  def self.create(name, grade)
+    Student.new(name, grade).tap do |new_student|
+      new_student.save
+    end
+  end
+
+  def self.new_from_db(row)
+   new_student = Student.new(row[0], row[1], row[2])
+   new_student
+ end
+
+ 
 
 
 end
