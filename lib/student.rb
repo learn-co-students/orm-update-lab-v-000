3859dda6df3ac80a700d8996cb09a5cfeac784d1
ryanwevans <ryanwevans@icloud.com>
2017-09-13 20:32:09 -0700

    added #find_by_name and #update methods. Lesson complete

--- lib/student.rb
+++ lib/student.rb
@@ -16,9 +16,9 @@ class Student
   def self.create_table
     sql = <<-SQL
       CREATE TABLE IF NOT EXISTS students (
-        id INTEGER PRIMARY KEY,
-        name TEXT,
-        grade TEXT
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      grade TEXT
       )
     SQL
 
@@ -54,7 +54,18 @@ class Student
   end
 
   def self.find_by_name(name)
-    
+    sql = <<-SQL
+      SELECT * FROM students WHERE name = ?
+    SQL
+    self.new_from_db(DB[:conn].execute(sql, name)[0])
+  end
+
+  def update
+    sql = <<-SQL
+      UPDATE students SET name = ?, grade = ? WHERE id = ?
+    SQL
+
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
   end
 
 end
