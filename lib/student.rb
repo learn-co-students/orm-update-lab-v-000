8cbde525e3ee7b3429d6fd33fbaca4d807622b61
zolkocarbon <eathena@hotmail.com>
2017-10-30 22:33:14 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -46,8 +46,7 @@ class Student
   end
 
   def self.new_from_db(row)
-    new_student = Student.new(row[0], row[1], row[2])
-    new_student
+    Student.new(row[0], row[1], row[2])
   end
 
   def self.find_by_name(name)
