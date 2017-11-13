556ab24edef092848c977809a0db9e3773a40799
bretburau <github email address>
2017-11-13 08:00:05 -0600

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,9 +1,66 @@
 require_relative "../config/environment.rb"
-
+  
 class Student
+  attr_accessor :name, :grade
+  attr_reader :id
+  def initialize(name, grade, id = nil)
+    @name = name
+    @grade = grade
+    @id = id
+  end
+  
+  def self.create_table
+    sql = <<-SQL
+    CREATE TABLE IF NOT EXISTS students(
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      grade TEXT
+      )
+    SQL
+    DB[:conn].execute(sql)
+  end
 
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]
+  def self.drop_table
+    sql = <<-SQL
+      DROP TABLE students
+    SQL
+    DB[:conn].execute(sql)
+  end
 
+  def save
+    if self.id
+      self.update
+    else
+      sql = <<-SQL
+        INSERT INTO students (name, grade) VALUES (?, ?)
+      SQL
+      DB[:conn].execute(sql, self.name, self.grade)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+    end
+  end
 
+  def update
+    sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
+  end
+
+  def self.create(name, grade)
+    student = self.new(name, grade)
+    student.save
+    student
+  end
+
+  def self.new_from_db(attributes)
+    student = self.new(attributes[1], attributes[2], attributes[0])
+    student
+  end
+
+  def self.find_by_name(name)
+    sql = "SELECT * FROM students WHERE name = ?"
+    row = DB[:conn].execute(sql, name).flatten
+    student = Student.new_from_db(row)
+    # binding.pry
+    student
+  end
+  
 end
