38e86e028b223b485c039f46052f1bb6cfdb7950
Olena Ageyeva <eageeva@hotmail.com>
2017-12-03 18:38:28 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,8 +1,8 @@
 require_relative "../config/environment.rb"
 
 class Student
-  attr_accessor :name,:grade
-  attr_reader :id
+  attr_accessor :name,:grade, :id
+
 
   def initialize(name,grade)
     @name, @grade= name, grade
@@ -44,11 +44,34 @@ class Student
     DB[:conn].execute(sql, self.name, self.grade, self.id)
   end
 
-  def self.create(name:, grade:)
+  def self.create(name, grade)
    student=Student.new(name,grade)
    student.save
    student
  end
+
+ def self.new_from_db(row)
+    # create a new Student object given a row from the database
+    student=create(row[1],row[2])
+    student.id=row[0]
+    student
+  end
+
+  def self.find_by_name(name)
+    # find the student in the database given a name
+    # return a new instance of the Student class
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE name = ?
+      LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
+
+  end
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
