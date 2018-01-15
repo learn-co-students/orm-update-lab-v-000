8a437342936aff15c5629557cda6a069941bc98c
Larry <github email address>
2018-01-15 05:55:25 -0500

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,9 +1,66 @@
 require_relative "../config/environment.rb"
 
 class Student
-
+  attr_accessor :name, :grade, :id
+  
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
-
+  def initialize(name, grade, id = nil)
+    @name = name
+    @grade = grade
+    @id = id
+  end
+  
+  def save
+    if self.id
+      self.update
+    else
+      sql = <<-SQL
+        INSERT INTO students (name, grade)
+        VALUES (?, ?) 
+      SQL
+      DB[:conn].execute(sql, @name, @grade)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+    end
+  end
+  
+  def update
+    sql = <<-SQL
+      UPDATE students
+      SET name = ?, grade = ? 
+      WHERE id = ?
+    SQL
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
+  end
+  
+  def self.create(name, grade)
+    Student.new(name, grade).save
+  end
+  
+  def self.new_from_db(arr)
+    Student.new(arr[1], arr[2], arr[0])
+  end
+  
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS students(
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade TEXT
+      )
+    SQL
+    DB[:conn].execute(sql)
+  end
+  
+  def self.find_by_name(name)
+    sql = "SELECT * FROM students WHERE name = ?"
+    Student.new_from_db (DB[:conn].execute(sql, name)[0])
+  end
+  
+  def self.drop_table
+    sql = "DROP TABLE students"
+    DB[:conn].execute(sql)
+  end
+  
 
 end
