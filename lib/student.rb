772366b234cd0777c7dfd4e9831c7d7015275077
wlycdgrfromflatiron <wlycdgrfromflatiron@gmail.com>
2018-01-31 22:15:47 -0500

    ALL TESTS PASSING

--- lib/student.rb
+++ lib/student.rb
@@ -29,4 +29,48 @@ class Student
 
     DB[:conn].execute(sql)
   end
+
+  def self.create(name, grade)
+    student = Student.new(name, grade)
+    student.save
+    student
+  end   
+  
+  def self.new_from_db(row)
+    Student.new(row[0], row[1], row[2])
+  end
+
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT * FROM students
+      WHERE name = ?
+    SQL
+
+    self.new_from_db(DB[:conn].execute(sql, name).first)
+  end
+
+  def save
+    if self.id
+      self.update
+    else
+      sql = <<-SQL
+        INSERT INTO students (name, grade)
+        VALUES (?, ?);
+      SQL
+
+      DB[:conn].execute(sql, self.name, self.grade)
+
+      @id = DB[:conn].execute('SELECT last_insert_rowid();').first.first
+    end
+  end
+
+  def update
+    sql = <<-SQL
+      UPDATE students
+      SET name = ?, grade = ?
+      WHERE id = ?;
+    SQL
+
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
+  end
 end
