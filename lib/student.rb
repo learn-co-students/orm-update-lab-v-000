8c394faf6515baafee8163f8d8260982ea7d6cdb
Araba Taylor <araba.taylor20@gmail.com>
2018-02-07 03:53:15 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -41,33 +41,31 @@ class Student
     DB[:conn].execute(sql, self.name, self.grade)
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end
+  end
 
-  def self.create(name:, grade:)
+  def self.create(name, grade)
     student = Student.new(name, grade)
     student.save
     student
   end
 
-  # def self.new_from_db(row)
-  #   new_student = self.new
-  #   new_student.id = row[0]
-  #   new_student.name = row[1]
-  #   new_student.grade = row[2]
-  #   new_student
-  # end
-  #
-  # def self.find_by_name(name)
-  #   sql = <<-SQL
-  #     SELECT *
-  #     FROM students
-  #     WHERE name = ?
-  #     LIMIT 1
-  #     SQL
-  #
-  #     DB[:conn].execute(sql, name).map do |row|
-  #       self.new_from_db(row)
-  #     end.first
-  # end
+  def self.new_from_db(row)
+    new_student = Student.new(row[0], row[1], row[2])
+    new_student.id = row[0]
+    new_student.name = row[1]
+    new_student.grade = row[2]
+    new_student
+  end
+
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT * FROM students WHERE name = ? LIMIT 1
+      SQL
+
+      DB[:conn].execute(sql, name).map do |row|
+        self.new_from_db(row)
+      end.first
+  end
 
   def update
     sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
@@ -77,5 +75,4 @@ class Student
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 
-end
 end
