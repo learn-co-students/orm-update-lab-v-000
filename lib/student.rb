de476d440eb707bbce8a86f41b79cc1f33d5d339
johnwojo <jwojo13@gmail.com>
2018-02-16 17:44:02 -0500

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,9 +1,34 @@
 require_relative "../config/environment.rb"
 
 class Student
+  attr_accessor :id, :name, :grade
 
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]
+  def initialize(id = nil, name, grade)
+    @name = name
+    @grade = grade
+  end
+
+  def self.create_table
+    "CREATE TABLE students"
+  end
+
+  def self.drop_table
+  end
+
+  def save
+  end
+
+  def create
+  end
+
+  def self.new_from_db
+  end
+
+  def self.find_by_name
+  end
+
+  def update
+  end
 
 
 end
