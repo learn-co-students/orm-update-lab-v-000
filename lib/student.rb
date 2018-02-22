aa64895bb00faa69ca24b812afe1b7f10750e2f8
Katrina Brinson <kbrinso@gmail.com>
2018-02-22 23:47:31 +0000

    completed through save method

--- lib/student.rb
+++ lib/student.rb
@@ -1,13 +1,49 @@
 require_relative "../config/environment.rb"
 
 class Student
-
+  attr_accessor :name, :grade, :id
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
-  def initialize(id=nil, name, breed)
+  def initialize(id=nil, name, grade)
   	@id = id
   	@name = name
-  	@breed = name
+  	@grade = grade
   end
 
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE students
+      (name, grade)
+    SQL
+
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = <<-SQL
+      DROP TABLE students
+    SQL
+
+    DB[:conn].execute(sql)
+  end
+
+  def save
+    # student = Student.new(self.name, self.grade)
+    # student
+    if self.id
+      sql = <<-SQL
+        UPDATE students
+        SET name = ?, grade = ?
+        WHERE id = ?
+      SQL
+      DB[:conn].execute(sql,self.name,self.grade, self.id)
+    else
+      sql = <<-SQL
+        INSERT INTO students (name, grade)
+        VALUES (?,?)
+      SQL
+      DB[:conn].execute(sql,self.name,self.grade)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+    end
+  end
 end
