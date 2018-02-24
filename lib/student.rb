c8fa3dbf2e5b17ef805ce824b6f803a58bb136f8
ElleeB <laura.trager@gmail.com>
2018-02-24 12:08:19 -0500

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -2,8 +2,7 @@ require_relative "../config/environment.rb"
 
 class Student
 
-  attr_accessor :name, :grade
-  attr_reader :id
+  attr_accessor :id, :name, :grade
 
   def initialize(id=nil, name, grade)
     @id = id
@@ -19,7 +18,50 @@ class Student
         grade INTEGER
       );
     SQL
+    DB[:conn].execute(sql)
   end
 
+  def self.drop_table
+    DB[:conn].execute("DROP TABLE students;")
+  end
+
+  def save
+    if self.id
+      self.update
+    else
+      sql = <<-SQL
+        INSERT INTO students (name, grade)
+        VALUES (?,?)
+      SQL
+      DB[:conn].execute(sql, self.name, self.grade)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+    end
+  end
+
+  def self.create(name, grade)
+    student = Student.new(name, grade)
+    student.save
+    student
+  end
+
+  def self.new_from_db(row)
+    student = Student.new(row[0],row[1], row[2])
+    student
+  end
+
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT * FROM students
+      WHERE name = ?
+    SQL
+    row = DB[:conn].execute(sql, name).flatten
+    student = Student.new_from_db(row)
+    student
+  end
+
+  def update
+    sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
+  end
 
 end
