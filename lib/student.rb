5aa0782d8811031fb05b00ec5b41f5f0e1c1b6e4
Carol Greene <carolgreene3@gmail.com>
2018-03-11 18:55:34 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -56,24 +56,37 @@ class Student
     student
   end
 
+  #or
+  #def self.new_from_db(row)
+    #id = row[0]
+    #name = row[1]
+    #grade = row[2]
+    #self.new(id, name, grade)
+  #end
+
   def self.find_by_name(name)
     sql = "SELECT * FROM students WHERE name = ?"
     result = DB[:conn].execute(sql, name)[0]
     Student.new(result[0], result[1], result[2])
   end
 
+  #or
+  #def self.find_by_name(name)
+    #sql = <<-SQL
+      #SELECT *
+      #FROM students
+      #WHERE name = ?
+      #LIMIT 1
+    #SQL
+
+    #DB[:conn].execute(sql,name).map do |row|
+      #self.new_from_db(row)
+    #end.first
+  #end
+
   def update
     sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
     DB[:conn].execute(sql, self.name, self.grade, self.id)
   end
-
-
-
-
-
-
-
-
-
 
 end
