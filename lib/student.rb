c72db8cd7a9dbebc293dc1cc90d176ad9c952451
Arye Dov Eidelman <aryedov.e@gmail.com>
2018-04-30 21:53:23 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,9 +1,38 @@
 require_relative "../config/environment.rb"
 
+# Remember, you can access your database connection anywhere in this class
+#  with DB[:conn]
+
 class Student
+  attr_accessor :id, :name, :grade
 
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]
+  def initialize(id = nil, name, grade)
+    @id = id
+    @name = name
+    @grade = grade
+  end
 
+  def save
+    sql = <<-SQL
+    INSERT INTO students(name, grade) VALUES (?, ?);
+    SQL
+    DB[:conn].execute(sql, name, grade)
+    @id = DB[:conn].execute("SELECT last_insert_rowid()")[0][0]
+    self
+  end
 
+  def self.create_table
+    sql = <<-SQL
+    CREATE TABLE students(
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      grade TEXT
+    );
+    SQL
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    DB[:conn].execute("DROP TABLE IF EXISTS students")
+  end
 end
