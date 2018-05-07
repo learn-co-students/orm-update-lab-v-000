b8ebd4e30ac4289a3f19e2668efbcc6ecea0a20b
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-05-07 21:28:40 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -41,4 +41,36 @@ class Student
   end
 end
 
+  def self.create(name, grade)
+    student = self.new(name, grade)
+    student.save
+    student
+  end
+  
+
+ def self.new_from_db(row)
+    student = self.new(row[1], row[2], row[0])
+    student
+  end
+  
+   def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT * FROM students
+      WHERE name = ?
+      LIMIT 1
+    SQL
+    DB[:conn].execute(sql, name).map do |row|
+    self.new_from_db(row)
+  end.first
+  end
+
+    def update
+    sql = <<-SQL
+      UPDATE students
+      SET name = ?, grade = ?
+      WHERE id = ?
+    SQL
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
+  end
+
 end
