45e441d50d8e37af946d6f628bce3c6d499e6c12
Dianne Jarvis-Lumsden <diannejarvisj@gmail.com>
2018-05-17 13:18:51 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -8,8 +8,9 @@ class Student
   attr_reader :id
 
   def initialize(id= nil, name, grade)
-    @name=name
-    @grade=grade
+    @id = id
+    @name = name
+    @grade = grade
   end
 
   def self.create_table
