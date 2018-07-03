ce71a48585bfa4a6dc08157511991ef5f0253662
Elaine Anderson <eparie@bu.edu>
2018-07-03 04:21:29 +0000

    completed

--- lib/student.rb
+++ lib/student.rb
@@ -1,9 +1,75 @@
+require 'pry'
 require_relative "../config/environment.rb"
 
 class Student
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
+  attr_accessor :name, :grade, :id
+
+
+def initialize(name, grade, id = nil)
+  @name = name
+  @grade = grade
+  @id = id
+end
+
+  def save
+    if self.id
+  self.update
+else
+  sql = <<-SQL
+    INSERT INTO students (name, grade)
+    VALUES (?, ?)
+  SQL
+
+  DB[:conn].execute(sql, self.name, self.grade)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+  end
+end
+
+def self.find_by_name(name)
+  sql = "SELECT * FROM students WHERE name = ?"
+  result = DB[:conn].execute(sql, name)[0]
+  student = Student.new(result[1], result[2], result[0])
+  student
+end
+
+def update
+  sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
+  DB[:conn].execute(sql, self.name, self.grade, self.id)
+end
+
+def self.create(name, grade)
+  student = self.new(name, grade)
+  student.save
+  student
+end
+
+def self.new_from_db(row)
+  new_student = self.new(:id, :name, :grade)
+new_student.id = row[0]
+new_student.name =  row[1]
+new_student.grade = row[2]
+new_student
+end
+
+def self.drop_table
+    sql = "DROP TABLE IF EXISTS students"
+    DB[:conn].execute(sql)
+  end
+
+  def self.create_table
+  sql = <<-SQL
+  CREATE TABLE IF NOT EXISTS students (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    grade TEXT
+  )
+  SQL
+
+  DB[:conn].execute(sql)
+end
 
 
 end
