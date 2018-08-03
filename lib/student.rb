d17c000b5087171e370159b3afef4d1c611621b7
Brittany Tinnin <bntinnin7@gmail.com>
2018-08-03 12:34:40 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -7,7 +7,7 @@ class Student
 
   attr_accessor :name, :grade, :id
 
-  def initialize(name, grade, id = nil)
+  def initialize(id = nil, name, grade)
     @id = id
     @name = name
     @grade = grade
@@ -41,12 +41,12 @@ class Student
       self.update
     else
       sql = <<-SQL
-        INSERT INTO students (name, grade) 
+        INSERT INTO students (name, grade)
         VALUES (?, ?)
       SQL
       DB[:conn].execute(sql, self.name, self.grade)
       @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
-    end 
+    end
   end
 
 
@@ -54,14 +54,21 @@ class Student
     Student.new(name, grade).save
   end
 
-  
-
 
   def self.new_from_db(row)
-    student = self.new(row[0], row[1], row[2])
-    student
+    self.new(row[0], row[1], row[2])
   end
 
-
+  def self.find_by_name(name)
+    sql = <<-SQL
+    SELECT *
+    FROM students
+    WHERE name = ?
+    LIMIT 1
+    SQL
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
 
 end
