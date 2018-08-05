b7408b7877f6f13cea0437a3e9fe6a695d721c11
paulheisele@gmail.com <paulheisele@gmail.com>
2018-08-05 22:06:43 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,9 +1,73 @@
 require_relative "../config/environment.rb"
 
 class Student
+  attr_accessor :id, :name, :grade
 
+  def initialize(name, grade, id = nil)
+    self.name, self.grade, self.id = name, grade, id
+  end
+
+  def self.create_table
+    sql = <<-SQL
+    CREATE TABLE IF NOT EXISTS students(
+    id INTEGER PRIMARY KEY,
+    name TEXT
+    grade INTEGER
+    );
+    SQL
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = <<-SQL
+    DROP TABLE students
+    SQL
+    DB[:conn].execute(sql)
+  end
+
+  def save
+    if self.id
+      update
+    else
+      sql = <<-SQL
+      INSERT INTO students (name, grade)
+      VALUES (?, ?)
+      SQL
+
+      DB[:conn].execute(sql, self.name, self.grade)
+      self.id = DB[:conn].execute("SELECT MAX(id) FROM students")[0][0]
+    end
+  end
+
+  def self.create(name, grade)
+    students = self.new(name, grade)
+    students.save
+  end
+
+  def self.new_from_db(row)
+    self.new(row[1], row[2], row[0])
+  end
+
+  def update
+    sql = <<-SQL
+    UPDATE students
+    SET name = ?, grade = ?
+    WHERE id = ?;
+    SQL
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
+  end
+
+  def self.find_by_name(name)
+    sql =<<-SQL
+    SELECT *
+    FROM students
+    WHERE name = ?
+    LIMIT 1;
+    SQL
+    row = DB[:conn].execute(sql, name)[0]
+    self.new_from_db(row)
+  end
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
-
 
 end
