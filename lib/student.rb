776f83238bd9de47d89e1f6c34df06a3a1593910
Ayana Bando <ayanaflax@yahoo.com>
2018-09-07 14:58:41 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -38,7 +38,6 @@ end
       SQL
       DB[:conn].execute(sql, self.name, self.grade)
       @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
-   
     end
   end
     
@@ -47,10 +46,32 @@ end
       DB[:conn].execute(sql, self.name, self.grade, self.id)
     end
   
+
   def self.create(name, grade)
     new_student = self.new(name, grade)
     new_student.save
    #binding.pry
   end
+  
+  def self.new_from_db(row)
+    #binding.pry
+    new_student = self.new(row, row)
+    new_student.id = row[0]
+    new_student.name =  row[1]
+    new_student.grade = row[2]
+    new_student
+  end
 
+    def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE name = ?
+      LIMIT 1
+    SQL
+ 
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
 end
