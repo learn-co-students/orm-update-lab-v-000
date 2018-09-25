60002daf1ca7580427ba9cac14f47dc21b14c9b6
Adam Barton <adam.barton333@gmail.com>
2018-09-25 05:33:50 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -67,9 +67,8 @@ class Student
     end
 
     def update
-      sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
-
-      DB[:conn].execute(sql, self.name, self,grade, self.id)
+      sql ="UPDATE students SET name = ?, grade = ? WHERE id = ?"
+      DB[:conn].execute(sql, self.name, self.grade, self.id)
     end
 
 
