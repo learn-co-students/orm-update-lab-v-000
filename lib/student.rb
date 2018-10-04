792755642490c02de82281dfcf50a47d9ea3bc9c
Justine Bricknell <justinegayton@gmail.com>
2018-10-04 22:14:59 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,4 @@
+require 'pry'
 require_relative "../config/environment.rb"
 
 class Student
@@ -39,18 +40,16 @@ class Student
     end
   end
 
-  def self.create(name:, grade:)
+  def self.create(name, grade)
     student = Student.new(name, grade)
     student.save
     student
   end
 
-  def self.new_from_db(row)
-    new_student = self.new
-    new_student.id = row[0]
-    new_student.name = row[1]
-    new_student.grade = row[2]
-    new_student #always return the object you just created here!!
+  def self.new_from_db(student_array)
+    #binding.pry
+    s = self.new(id = student_array[0], name = student_array[1], grade = student_array[2])
+    s #always return the object you just created here!!
   end
 
   def self.find_by_name(name)
