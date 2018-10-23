d2caebb5fa738571525301ed5fd21275e7ea8248
Laura Messa Travin <laura.travin@gmail.com>
2018-10-23 21:40:37 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -52,7 +52,7 @@ end
 
 #read db and create and instance
 def self.new_from_db(row)
-   new_student = self.new(row[1],row[2],row[0])
+   new_student = self.new(row[1],row[2],row[0]) #must check the order in initialize
    new_student
 end
 
@@ -64,7 +64,7 @@ end
 def self.find_by_name(name)
     sql = "SELECT * FROM students WHERE name = ?"
     result = DB[:conn].execute(sql, name)[0]
-    Student.new( result[1], result[2], result[0])
+    Student.new( result[1], result[2], result[0])  #must check the order in initialize
 end
 
 
