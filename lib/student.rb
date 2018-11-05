28a74f30371b3506e7812d3adf9c64fedd590397
smithwebtek <brad@smithwebtek.com>
2018-11-05 14:36:58 -0500

    Update lab

--- lib/student.rb
+++ lib/student.rb
@@ -3,11 +3,11 @@ require_relative "../config/environment.rb"
 class Student
   attr_accessor :name, :grade
   attr_reader :id
-
+	
   def initialize(name, grade, id=nil)
     @name = name
     @grade = grade
-    @id = id
+		@id = id
   end
 
   def self.create_table
