87428b6842e0f395c049e678f8b0373ff7d2226f
Azhar Shah <azharshahstl@yahoo.com>
2018-11-30 20:11:12 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -63,6 +63,18 @@ class Student
     student.grade = row[2]
     student
   end
+   
+   def self.find_by_name(name) 
+    sql = <<-SQL
+    SELECT * FROM students
+    WHERE name = ?
+    LIMIT 1
+    SQL
+    
+    DB[:conn].execute(sql,name).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
     
 
   # Remember, you can access your database connection anywhere in this class
