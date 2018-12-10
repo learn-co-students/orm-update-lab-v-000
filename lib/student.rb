dcc54b14cd7e939abbf152a48ad46394178cad74
Cheryl K <cherylkelson@icloud.com>
2018-12-10 17:08:37 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -2,7 +2,9 @@ require 'pry'
 require_relative "../config/environment.rb"
 
 class Student
-  attr_accessor :name, :grade, :id
+
+  attr_accessor :name, :grade
+  attr_reader :id
 
   def initialize(id = nil, name, grade)
     @id = id
@@ -10,8 +12,7 @@ class Student
     @grade = grade
 
   end
-  # Remember, you can access your database connection anywhere in this class
-  #with DB[:conn]
+
 
   def self.create_table
     sql = <<-SQL
@@ -23,6 +24,7 @@ class Student
     SQL
     DB[:conn].execute(sql)
   end
+
 
   def self.drop_table
    sql = "DROP TABLE IF EXISTS students"
@@ -45,16 +47,45 @@ class Student
   end
 
 
-  def create
+  def self.create(name, grade)
+    student = Student.new(name, grade)
+    student.save
+    student
   end
 
-  def new_from_db
+
+  def self.new_from_db(row)
+    id = row[0]
+    name = row[1]
+    grade = row[2]
+    self.new(id, name, grade)
   end
 
-  def find_by_name
+
+  def self.find_by_name(name, id) 
+    sql = "SELECT *
+    FROM students
+    WHERE name = ?"
+    row = DB[:conn].execute(sql, name)
+    self.find_by_name
+
   end
+
 
   def update
-  end
+    sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
+#binding.pry
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
+
+    # This method updates the database row mapped to the given `Student` instance.
+    # describe '#update' do
+      #it 'updates the record associated with a given instance' do
+        #josh.save
+        #josh.name = "Josh Jr."
+        #josh.update
+        #josh_jr = Student.find_by_name("Josh Jr.")
+        #expect(josh_jr.id).to eq(josh.id)
+
+    end
 
 end
