af48f0870154a50b226bdc5683e59f617cd94fdb
Nick Romans <nicholas.romans@gmail.com>
2019-01-05 18:38:52 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -24,21 +24,60 @@ class Student
   end
 
   def self.drop_table
+    sql = <<-SQL
+    DROP TABLE students
+    SQL
+
+    DB[:conn].execute(sql)
   end
 
   def save
+    if self.id
+      self.update
+    else
+      sql = <<-SQL
+      INSERT INTO students (name, grade)
+      VALUES (?, ?)
+      SQL
+      DB[:conn].execute(sql, self.name, self.grade)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+    end
   end
 
-  def self.create
+  def self.create(name, grade)
+    student = Student.new(name, grade)
+    student.save
+    student
   end
 
-  def self.new_from_db
+  def self.new_from_db(row)
+    id = row[0]
+    name = row[1]
+    grade = row[2]
+    new_student = self.new(id, name, grade)
   end
 
-  def self.find_by_name
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE name = ?
+      LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql, name).map do |row|
+    self.new_from_db(row)
+    end.first
   end
 
   def update
+    sql = <<-SQL
+    UPDATE students
+    SET name = ?, grade = ?
+    WHERE id = ?
+    SQL
+
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
   end
 
   # Remember, you can access your database connection anywhere in this class
