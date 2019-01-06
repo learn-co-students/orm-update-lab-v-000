d5cbf7b1c7d3a27a80d8f003566314ed1a05a072
Ashley Wong <ashwong04@gmail.com>
2019-01-06 22:24:44 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -28,9 +28,23 @@ class Student
   end
   
   def save
-    
+    if self.id
+      self.update    
+    else
+      sql = <<-SQL
+        INSERT INTO students (name, grade)
+        VALUES (?, ?)
+      SQL
+ 
+    DB[:conn].execute(sql, self.name, self.grade)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+    end
   end
   
+  def update
+    sql = "UPDATE songs SET name = ?, album = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.album, self.id)
+  end
   
   
   
