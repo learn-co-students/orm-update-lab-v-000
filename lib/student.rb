5179c6718503e019d383c96745ce5d7a1b0c02bf
Ashley Wong <ashwong04@gmail.com>
2019-01-08 01:18:25 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -53,7 +53,12 @@ class Student
   end
   
   def self.new_from_db(row)
-    
+    self.new(row[1], row[2], row[0])
+  end
+  
+  def self.find_by_name(name)
+    row =DB[:conn].execute("SELECT * FROM students WHERE name = ?", name)
+    self.new_from_db(row[0])
   end
   
 end
