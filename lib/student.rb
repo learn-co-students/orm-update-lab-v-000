3e6651c67a2b8dc56c392e4d96dab0c8f6588e72
Mariel Hoepelman <marielhoepelman@gmail.com>
2019-03-10 20:57:42 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -9,21 +9,19 @@ class Student
       @grade = grade
     end
 
-    def self.create_table
-   sql = <<-SQL
-   CREATE TABLE IF NOT EXISTS students (
-     id INTEGER PRIMARY KEY,
-     name TEXT,
-     grade TEXT
-   )
-   SQL
-   DB[:conn].execute(sql)
- end
+  def self.create_table
+    sql = "CREATE TABLE IF NOT EXISTS students (
+       id INTEGER PRIMARY KEY,
+       name TEXT,
+       grade TEXT
+    )"
+    DB[:conn].execute(sql)
+  end
 
- def self.drop_table
+  def self.drop_table
    sql = "DROP TABLE IF EXISTS students"
    DB[:conn].execute(sql)
- end
+  end
 
   def update
     sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
@@ -34,10 +32,9 @@ class Student
    if self.id
      self.update
   else
-   sql = <<-SQL
-     INSERT INTO students (name, grade)
-     VALUES (?, ?)
-   SQL
+   sql = "INSERT INTO students (name, grade)
+     VALUES (?, ?)"
+
    DB[:conn].execute(sql, self.name, self.grade)
    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end
@@ -49,24 +46,14 @@ class Student
     student
   end
 
-  #def self.new_from_db(row)
-   #new_student = self.new
-   #new_student.id = row[0]
-   #new_student.name =  row[1]
-   #new_student.grade = row[2]
-   #new_student
- #end
-
   def self.new_from_db(row)
-    id = row[0]
-    name = row[1]
-    grade = row[2]
+    id, name, grade = row[0], row[1], row[2]
     self.new(id, name, grade)
   end
 
   def self.find_by_name(name)
    sql = "SELECT * FROM students WHERE name = ?"
-   result = DB[:conn].execute(sql, name)[0]
-   Student.new(result[0], result[1], result[2])
+   row = DB[:conn].execute(sql, name)
+   self.new(row[0][0], row[0][1], row[0][2])
   end
 end
