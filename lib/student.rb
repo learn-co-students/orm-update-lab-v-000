113a629efffe8f70298faea2b2a17325a81c529b
Mariel Hoepelman <marielhoepelman@gmail.com>
2019-03-10 20:58:24 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -31,9 +31,9 @@ class Student
  def save
    if self.id
      self.update
-  else
-   sql = "INSERT INTO students (name, grade)
-     VALUES (?, ?)"
+   else
+     sql = "INSERT INTO students (name, grade)
+       VALUES (?, ?)"
 
    DB[:conn].execute(sql, self.name, self.grade)
    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
