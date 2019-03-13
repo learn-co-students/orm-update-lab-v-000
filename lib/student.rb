c4d84041ed8e2dc2d4c272d4d9d587f7edf38945
matthew.serre <matthew.serre@gmail.com>
2019-03-13 19:36:55 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -14,7 +14,7 @@ class Student
   
   def self.create_table
     sql = <<-SQL
-      CREATE TABLE students IF NOT EXISTS (
+      CREATE TABLE IF NOT EXISTS students (
       id INTEGER PRIMARY KEY,
       name TEXT,
       grade INTEGER
@@ -31,23 +31,45 @@ class Student
   end
   
   def save
-    
+    if self.id
+      self.update
+    else
+      sql = <<-SQL
+        INSERT INTO students (name, grade)
+        VALUES (?, ?)
+      SQL
+      DB[:conn].execute(sql, self.name, self.grade)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+    end
   end
   
-  def self.create
-    
+  def self.create(name, grade)
+    student = Student.new(name, grade)
+    student.save
+    student
   end
   
-  def self.new_from_db
-    
+  def self.new_from_db(row)
+    student = self.new(row[0],row[1],row[2])
+    student
   end
   
-  def self.find_by_name
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT * FROM students WHERE name = ? LIMIT 1
+    SQL
     
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end
   end
   
   def update
+    sql = <<-SQL
+      UPDATE students set name = ?, grade = ?, id = ?
+    SQL
     
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
   end
 
 end
