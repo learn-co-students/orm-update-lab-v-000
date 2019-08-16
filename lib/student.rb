e0d4560b442724c4ac6a947950084bc5868cc1d6
Amar Chatman <amarjerome@gmail.com>
2019-08-16 07:07:55 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -29,7 +29,33 @@ attr_accessor :name, :grade, :id
   end
 
  def save
- end 
+   if self.id
+     self.update
+   else
+     sql = <<-SQL
+       INSERT INTO students (name, grade)
+       VALUES (?, ?)
+     SQL
+     DB[:conn].execute(sql, self.name, self.grade)
+     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+   end
+ end
+
+
+  def update
+    sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
+  end
+
+def self.create(name,grade)
+  student = Student.new(name,grade)
+  student.save
+  student
+end
+
+def self.new_from_db
+end 
+
 
 end
 #  with DB[:conn]
