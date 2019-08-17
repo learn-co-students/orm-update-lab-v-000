b78a8374fa996ffabe60b691f2e2482fffde6dad
Amar Chatman <amarjerome@gmail.com>
2019-08-17 18:53:57 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,4 +1,5 @@
 require_relative "../config/environment.rb"
+require 'pry'
 
 class Student
 
@@ -55,10 +56,15 @@ def self.create(name,grade)
 end
 
 def self.new_from_db(array)
-  student_attributes = array.join(',')
-  Student.new(student_attributes)
+  #binding.pry
+  Student.new(array[0], array[1], array[2])
 end
 
+def self.find_by_name(name)
+  sql = "SELECT * FROM students WHERE name = ?"
+result = DB[:conn].execute(sql, name)[0]
+Student.new(result[0], result[1], result[2])
+end
 
 end
 #  with DB[:conn]
