abcf19e88823192c32554dc5631b1c74d0e44e19
alejandra cantu <alejandra.cantu13@gmail.com>
2020-02-12 21:21:29 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -4,6 +4,10 @@ class Student
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
-
+  def update
+     sql = "UPDATE students SET name = ?, grade = ? WHERE name = ?"
+     DB[:conn].execute(sql, self.name, self.grade, self.name)
+   end
+ end
 
 end
